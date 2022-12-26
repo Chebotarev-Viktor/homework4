803d5a036e29faf5cea706d8c2fc86bea49388fd
abc <abc@example.com>
2022-12-26 13:29:14 +0500

    first commit

---
+++ task1/Program.cs
@@ -0,0 +1,51 @@
+﻿
+int numA = 0;
+int numB = 0;
+int sum = 0;
+
+while(true)
+{
+    Console.WriteLine("Введите целое число А:");
+
+    if(int.TryParse(Console.ReadLine(), out int number))
+    {
+        numA = number; break;
+    }
+    else Console.WriteLine("некорректный ввод");
+}
+
+while(true)
+{
+    Console.WriteLine(" число В:");
+
+    if(int.TryParse(Console.ReadLine(), out int number))
+    {
+        if(number < 0) 
+        {
+            Console.WriteLine("некоректный ввод");
+            continue;
+        }
+        else numB = number; break;
+    }
+    else Console.WriteLine("некоректный ввод");
+}
+
+Console.WriteLine("число " + numA + " в степени " + numB + " = " + Calculate(numA, numB));
+
+
+int Calculate(int a, int b)
+{
+    if (a == 0 && b == 0)
+    {
+        sum = 1;
+    }
+    else
+    {
+        sum = a;
+        for(int i = 1; i < b; i++)
+        {
+            sum = sum * a;
+        }
+    }
+    return sum;
+}
---
+++ task2/Program.cs
@@ -0,0 +1,18 @@
+﻿Console.WriteLine ("Введите число: ");
+int numberN = int.Parse(Console.ReadLine());
+
+  int SumNumber(int numberN)
+  {
+    int counter = Convert.ToString(numberN).Length;
+    int progress = 0;
+    int result = 0;
+    for (int i = 0; i < counter; i++)
+    {
+      progress = numberN - numberN % 10;
+      result = result + (numberN - progress);
+      numberN = numberN / 10;
+    }
+   return result;
+  }
+int sumNumber = SumNumber(numberN);
+Console.WriteLine(sumNumber);
